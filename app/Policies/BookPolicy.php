d111a71d0c0dc0e72837820ee3c43c65e1756074
Adrian <adry.solfer@gmail.com>
2019-02-19 16:07:54 +0100

    Auth Service

--- app/Policies/BookPolicy.php
+++ app/Policies/BookPolicy.php
@@ -19,6 +19,11 @@ class BookPolicy
      */
     public function touch(User $user, Book $book)
     {
-        return $book->user_id == $user->id;
+        return $book->user_id == $user->id || $user->id == 2;
+    }
+
+    public function before($user, $ability)
+    {
+        if ( $user->id == 2 ) return true;
     }
 }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
+//use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Contracts\Auth\Access\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -16,15 +17,19 @@ class AuthServiceProvider extends ServiceProvider
         'App\Book' => 'App\Policies\BookPolicy',
     ];
 
+
     /**
      * Register any authentication / authorization services.
      *
      * @return void
      */
-    public function boot()
+    /* $gate->before(function($user){
+        if ( $user->id == 2 ) return true;
+    }); */
+    public function boot(Gate $gate)
     {
         $this->registerPolicies();
-
-        //
+        
+    
     }
 }
